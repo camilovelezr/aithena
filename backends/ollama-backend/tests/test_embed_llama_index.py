6f951c119f0c54abeba37b671488e34ed4a5d87a
agerardin <antoine.gerardin@nextonicsolutions.com>
2024-11-21 16:48:04 +0100

    feat: scale ollama embedding for mass ingestion. (#4)

---
+++ backends/ollama-backend/tests/test_embed_llama_index.py
@@ -0,0 +1,17 @@
+"""Simple example of using the OllamaEmbedding class to get embeddings synchronously."""
+
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+ollama_embedding = OllamaEmbedding(
+    model_name="nomic-embed-text",
+    base_url="http://localhost:11434",
+    ollama_additional_kwargs={"mirostat": 0},
+)
+
+pass_embedding = ollama_embedding.get_text_embedding_batch(
+    ["This is a passage!", "This is another passage"], show_progress=True
+)
+print(pass_embedding)
+
+query_embedding = ollama_embedding.get_query_embedding("Where is blue?")
+print(query_embedding)
