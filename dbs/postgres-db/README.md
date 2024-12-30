f4819220fc18c51ea672895861820735da050b65
Antoine Gerardin <antoine.gerardin@nextonicsolutions.com>
2024-12-30 07:48:00 +0000

    feat: custom postgres configuration for large scale deployment.

--- dbs/postgres-db/README.md
+++ dbs/postgres-db/README.md
@@ -9,19 +9,40 @@ This projects provide deployments for postgres using:
 Copy and rename `.env-sample` to `.env` and update values according
 to your configuration.
 
+Create a secret from this `.env` file
+
+```shell
+kubectl create secret generic aithenadb-secret --from-env-file=.env -n namespace
+```
+
 ## Deploy 
+
+### Regular Deployment
 
 ```shell
 helm dependency update
 helm install ${RELEASE} . -n ${NAMESPACE}
 ```
 
+### Large Scale Deployment
+
+Rename `custom-depl-sample.yaml` to `custom-depl.yaml`
+
+Update config and deply :
+
+```shell
+kubectl apply -f custom-depl.yaml
+```
+
 ## Admin
 
+```
 docker pull dpage/pgadmin4
+```
 
+```
 docker run -p 30980:80 \
     -e 'PGADMIN_DEFAULT_EMAIL=${ADMIN_EMAIL}' \
     -e 'PGADMIN_DEFAULT_PASSWORD=${ADMIN_PASSWORD}' \
     -d dpage/pgadmin4 
-
+```
--- dbs/postgres-db/scripts/test_duplicate_ids.py
+++ dbs/postgres-db/scripts/test_duplicate_ids.py
@@ -7,12 +7,10 @@ This script will query the database to find any duplicate IDs in the works table
 
 from dotenv import load_dotenv
 import os
-import orjson
 from polus.aithena.common.utils import time_logger
 import psycopg
 from polus.aithena.common.logger import get_logger
-from openalex_types.works import Work, _construct_abstract_from_index
-import requests
+
 
 logger = get_logger(__name__)
 
@@ -29,7 +27,6 @@ port = os.getenv("POSTGRES_PORT")
 logger.info(f"Connecting to database {dbname} on {host}:{port} as user {user}")
 
 def db_connection():
-    # Connect to your PostgreSQL database
     conn = psycopg.connect(
         dbname=dbname,
         user=user,
@@ -38,42 +35,14 @@ def db_connection():
         port=port
     )
     yield conn
-
-    # Close the connection
-    logger.debug("!!!!!!!!! Closing database connection")
     conn.close()
 
-AITHENA_SERVICES_URL = os.getenv("AITHENA_SERVICES_URL", "http://localhost:8000")
-EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "nomic-embed-text")
-EMBED_URL = f"{AITHENA_SERVICES_URL}/embed/{EMBEDDING_MODEL}/generate"
-DEFAULT_HEADERS = { "Content-Type": "application/json"}
-
 @time_logger
-def run_query():
-    schema_name = "openalex"
-    table_name = "works"
+def test_duplicate_ids(schema_name="openalex", table_name="works"):
 
     conn = next(db_connection())
     cur = conn.cursor()
 
-    # Execute the query to get the column names
-    cur.execute(f"""
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = '{table_name}'
-        AND table_schema = '{schema_name}';
-    """)
-
-    # Fetch all results
-    columns = cur.fetchall()
-
-    logger.debug(f"""{len(columns)} columns found in the {table_name} table""")
-
-    # Print the list of column names
-    for column in columns:
-        print(column[0])
-
-    # # Execute the query to get the first 10 records from the openalex.works table
     cur.execute(f"""
         SELECT id, COUNT(*)
         FROM {schema_name}.{table_name}
@@ -84,33 +53,13 @@ def run_query():
     # Fetch all results
     records = cur.fetchall()
     if len(records) == 0:
-        logger.info("No duplicate IDs found")
+        logger.info("No duplicate IDs found for schema: {schema_name}, table: {table_name}")
+    else:
 
-    for record in records:
-        logger.debug(f"duplicate record (id, count): {record}")
-
-
-def explain_query():
-    schema_name = "openalex"
-    table_name = "works"
-
-    conn = next(db_connection())
-    cur = conn.cursor()
-
-    # Execute the query to get the first 10 records from the openalex.works table
-    cur.execute(f"""
-        EXPLAIN ANALYZE
-        SELECT id, COUNT(*)
-        FROM {schema_name}.{table_name}
-        GROUP BY id
-        LIMIT 10;
-    """)
-
-    # Fetch and print the query plan
-    query_plan = cur.fetchall()
-    for line in query_plan:
-        print(line)
-
+        for record in records:
+            logger.debug(f"duplicate record (id, count): {record}")
+        
+        raise Exception("Duplicate IDs found for schema: {schema_name}, table: {table_name}")
+    
 if __name__ == "__main__":
-    run_query()
-    # explain_query()
+    test_duplicate_ids(schema_name = "openalex", table_name = "works")
