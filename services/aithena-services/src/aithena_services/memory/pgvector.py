fc6578d07c2375ec827918c7ac37d169927c9cf9
agerardin <antoine.gerardin@nextonicsolutions.com>
2024-12-30 16:54:16 +0100

    Feat/full_work (#20)
    * feat: add full work object query
    
    * fix: dev dependency on openalex, bumpversion.
    
    ---------
    
    Co-authored-by: Camilo Velez <camilov61@gmail.com>

--- services/aithena-services/src/aithena_services/memory/pgvector.py
+++ services/aithena-services/src/aithena_services/memory/pgvector.py
@@ -17,26 +17,13 @@ from polus.aithena.common.logger import get_logger
 logger = get_logger("aithena_services.memory.pgvector")
 
 DB_CONFIG_STRING = f"dbname={POSTGRES_DB} user={POSTGRES_USER} "
-DB_CONFIG_STRING += f"port={POSTGRES_PORT} host={POSTGRES_HOST} password={POSTGRES_PASSWORD}"
+DB_CONFIG_STRING += (
+    f"port={POSTGRES_PORT} host={POSTGRES_HOST} password={POSTGRES_PASSWORD}"
+)
 
-def similarity_search(
-    table_name: str,
-    vector: list[float],
-    limit: int,
-) -> list[Work]:
-    """
-    Search for similar vectors in a table.
 
-    Args:
-        table_name (str): The name of the table to search in.
-        vector_column (str): The name of the column containing the vectors.
-        return_column (str): The name of the column to return in the results.
-        vector (list[float]): The vector to search for similarities.
-        limit (int, optional): The maximum number of similar vectors to return.
-
-    Returns:
-        list[str]: A list of IDs of the similar vectors found.
-    """
+def return_query_full_work(table_name: str, vector: list[float], limit: int) -> str:
+    """Return query for full work."""
     query = f"""
     WITH limited_works AS (
     SELECT works.id
@@ -45,31 +32,139 @@ def similarity_search(
         ON emb.work_id = works.id
     ORDER BY emb.embedding <=> '{vector}'
     LIMIT {limit}
-    )
+    ) --- CTE: do this first to limit the number of works
     SELECT
-        row_to_json(works)::jsonb || jsonb_build_object(
-            'authorships',
-            CASE
-                WHEN COUNT(works_authorships.work_id) = 0 THEN NULL
-                ELSE json_agg(
-                    DISTINCT json_build_object(
-                        'author', json_build_object(
-                            'display_name', authors.display_name,
-                            'id', works_authorships.author_id
-                        )::jsonb
-                    )::jsonb
+        jsonb_agg(
+            jsonb_strip_nulls(
+                row_to_json(works)::jsonb || jsonb_build_object(
+                    'primary_location', (
+                        SELECT row_to_json(works_primary_locations)::jsonb
+                        FROM openalex.works_primary_locations AS works_primary_locations
+                        WHERE works_primary_locations.work_id = works.id
+                    ),
+                    'locations', (
+                        SELECT jsonb_agg(row_to_json(works_locations)::jsonb)
+                        FROM openalex.works_locations AS works_locations
+                        WHERE works_locations.work_id = works.id
+                    ),
+                    'best_oa_location', (
+                        SELECT row_to_json(works_best_oa_location)::jsonb
+                        FROM openalex.works_best_oa_locations AS works_best_oa_location
+                        WHERE works_best_oa_location.work_id = works.id
+                    ),
+                    'authorships', (
+                        SELECT jsonb_agg(row_to_json(works_authorships)::jsonb)
+                        FROM openalex.works_authorships AS works_authorships
+                        WHERE works_authorships.work_id = works.id
+                    ),
+                    'biblio', (
+                        SELECT row_to_json(works_biblio)::jsonb
+                        FROM openalex.works_biblio AS works_biblio
+                        WHERE works_biblio.work_id = works.id
+                    ),
+                    'topics', (
+                        SELECT jsonb_agg(row_to_json(works_topics)::jsonb)
+                        FROM openalex.works_topics AS works_topics
+                        WHERE works_topics.work_id = works.id
+                    ),
+                    'concepts', (
+                        SELECT jsonb_agg(row_to_json(works_concepts)::jsonb)
+                        FROM openalex.works_concepts AS works_concepts
+                        WHERE works_concepts.work_id = works.id
+                    ),
+                    'ids', (
+                        SELECT row_to_json(works_ids)::jsonb
+                        FROM openalex.works_ids AS works_ids
+                        WHERE works_ids.work_id = works.id
+                    ),
+                    'mesh', (
+                        SELECT jsonb_agg(row_to_json(works_mesh)::jsonb)
+                        FROM openalex.works_mesh AS works_mesh
+                        WHERE works_mesh.work_id = works.id
+                    ),
+                    'open_access', (
+                        SELECT row_to_json(works_open_access)::jsonb
+                        FROM openalex.works_open_access AS works_open_access
+                        WHERE works_open_access.work_id = works.id
+                    ),
+                    'referenced_works', (
+                        SELECT jsonb_agg(row_to_json(works_referenced_works)::jsonb)
+                        FROM openalex.works_referenced_works AS works_referenced_works
+                        WHERE works_referenced_works.work_id = works.id
+                    ),
+                    'related_works', (
+                        SELECT jsonb_agg(row_to_json(works_related_works)::jsonb)
+                        FROM openalex.works_related_works AS works_related_works
+                        WHERE works_related_works.work_id = works.id
+                    )
                 )
-            END
-        ) AS work_with_authorships
+            )
+        ) AS full_work
     FROM limited_works
     JOIN openalex.works AS works
         ON limited_works.id = works.id
-    LEFT JOIN openalex.works_authorships AS works_authorships
-        ON works.id = works_authorships.work_id
-    LEFT JOIN openalex.authors AS authors
-        ON works_authorships.author_id = authors.id
-    GROUP BY works.id;  
     """
+    return query
+
+
+def similarity_search(
+    table_name: str,
+    vector: list[float],
+    limit: int,
+    full: bool = False,
+) -> list[Work]:
+    """
+    Search for similar works.
+
+    If full is set to True, the full Work object is returned.
+    If full is set to False, only the basic work metadata with
+    authorships is returned.
+
+    Args:
+        table_name (str): The name of the table to search in.
+        vector (list[float]): The vector to search for similarities.
+        limit (int, optional): The maximum number of similar vectors to return.
+        full (bool, optional): Whether to query and return the full Work object.
+
+    Returns:
+        list[Work]: A list of Work objects as dict.
+    """
+    if not full:
+        query = f"""
+        WITH limited_works AS (
+        SELECT works.id
+        FROM {table_name} AS emb
+        JOIN openalex.works AS works
+            ON emb.work_id = works.id
+        ORDER BY emb.embedding <=> '{vector}'
+        LIMIT {limit}
+        )
+        SELECT
+            row_to_json(works)::jsonb || jsonb_build_object(
+                'authorships',
+                CASE
+                    WHEN COUNT(works_authorships.work_id) = 0 THEN NULL
+                    ELSE json_agg(
+                        DISTINCT json_build_object(
+                            'author', json_build_object(
+                                'display_name', authors.display_name,
+                                'id', works_authorships.author_id
+                            )::jsonb
+                        )::jsonb
+                    )
+                END
+            ) AS work_with_authorships
+        FROM limited_works
+        JOIN openalex.works AS works
+            ON limited_works.id = works.id
+        LEFT JOIN openalex.works_authorships AS works_authorships
+            ON works.id = works_authorships.work_id
+        LEFT JOIN openalex.authors AS authors
+            ON works_authorships.author_id = authors.id
+        GROUP BY works.id;  
+        """
+    else:
+        query = return_query_full_work(table_name, vector, limit)
 
     with psycopg.connect(DB_CONFIG_STRING) as conn:
         with conn.cursor() as cur:
@@ -80,22 +175,26 @@ def similarity_search(
                 cur.execute(query)
                 res = cur.fetchall()
 
-                print("!!!!!!!!!!!! res", res)
+                logger.debug(f"!!!!!!!!!!!! res {res}")
 
-                works = [Work(**w[0]) for w in res]
+                if full:
+                    works = [Work(**w) for w in res[0][0]]
+                else:
+                    works = [Work(**w[0]) for w in res]
             except Exception as e:
                 logger.error(f"Error when performing similarity search: {e}")
                 raise e
 
     return works
 
+
 def work_ids_by_similarity_search(
     table_name: str,
     vector: list[float],
     limit: int,
-) -> list[Work]:
+) -> list[str]:
     """
-    Search for similar vectors in a table.
+    Search for similar works and return only id.
 
     Args:
         table_name (str): The name of the table to search in.
@@ -106,9 +205,9 @@ def work_ids_by_similarity_search(
         list[str]: A list of IDs of the similar vectors found.
     """
     query = f"""
-SELECT work_id
-FROM {table_name}
-ORDER BY embedding <=> '{vector}' LIMIT {limit};
+    SELECT work_id
+    FROM {table_name}
+    ORDER BY embedding <=> '{vector}' LIMIT {limit};
     """
 
     with psycopg.connect(DB_CONFIG_STRING) as conn:
@@ -119,11 +218,13 @@ ORDER BY embedding <=> '{vector}' LIMIT {limit};
                     f"Performing similarity search with query: {query_to_log}")
                 cur.execute(query)
                 res = cur.fetchall()
+                res_to_return = [r[0] for r in res]
             except Exception as e:
                 logger.error(f"Error when performing similarity search: {e}")
                 raise e
 
-    return res
+    return res_to_return
+
 
 def works_by_similarity_search(
     table_name: str,
@@ -131,7 +232,7 @@ def works_by_similarity_search(
     limit: int,
 ) -> list[Work]:
     """
-    Search for similar vectors in a table.
+    Search for similar works and return just basic work metadata.
 
     Args:
         table_name (str): The name of the table to search in.
@@ -142,15 +243,15 @@ def works_by_similarity_search(
         list[str]: A list of IDs of the similar vectors found.
     """
     query = f"""
-WITH selected_work_ids AS (
-SELECT work_id
-FROM {table_name}
-ORDER BY embedding <=> '{vector}' LIMIT {limit}
-)
-SELECT w.*
-FROM openalex.works w
-JOIN selected_work_ids s ON w.id = s.work_id;
-;
+    WITH selected_work_ids AS (
+    SELECT work_id
+    FROM {table_name}
+    ORDER BY embedding <=> '{vector}' LIMIT {limit}
+    )
+    SELECT w.*
+    FROM openalex.works w
+    JOIN selected_work_ids s ON w.id = s.work_id;
+    ;
     """
 
     with psycopg.connect(DB_CONFIG_STRING) as conn:
